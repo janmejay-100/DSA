ac906ced8b3d696d20eb197481f9ce866df3440e
Janmejay Sahoo <janmejaysahoo100@gmail.com>
2026-09-18 10:36:52 +0530

    [Syncode] LeetCode/Easy/Linked List Cycle

---
+++ LeetCode/Easy/Linked List Cycle/solution.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        // if(head==null || head.next==null){
+        //     return false;
+        // }
+        // ListNode fast=head;
+        // ListNode slow=head;
+        // while(fast!=null && fast.next!=null){
+        //     fast=fast.next.next;
+        //     slow=slow.next;
+        //     if(fast==slow){
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        Set <ListNode> set= new HashSet<>();
+        ListNode curr=head;
+        while(curr!=null){
+            if(set.contains(curr)){
+                return true;
+            }
+            set.add(curr);
+            curr=curr.next;
+        }
+        return false;
+    }
+}
--- README.md
+++ README.md
@@ -77,3 +77,4 @@
 | 75 | [Reverse Linked List](./LeetCode/Easy/Reverse%20Linked%20List) | [LeetCode](https://leetcode.com/problems/reverse-linked-list/) | Easy | 17 Sept 2026 | 08:48 pm |
 | 76 | [Merge Two Sorted Lists](./LeetCode/Easy/Merge%20Two%20Sorted%20Lists) | [LeetCode](https://leetcode.com/problems/merge-two-sorted-lists/) | Easy | 17 Sept 2026 | 09:00 pm |
 | 77 | [Palindrome Linked List](./LeetCode/Easy/Palindrome%20Linked%20List) | [LeetCode](https://leetcode.com/problems/palindrome-linked-list/) | Easy | 18 Sept 2026 | 10:29 am |
+| 78 | [Linked List Cycle](./LeetCode/Easy/Linked%20List%20Cycle) | [LeetCode](https://leetcode.com/problems/linked-list-cycle/) | Easy | 18 Sept 2026 | 10:36 am |
